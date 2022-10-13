49762cf1fd417ab9711acef8088a8feafc2ae57f
Bryanzef <98066200+Bryanzef@users.noreply.github.com>
2022-10-13 14:56:11 -0300

    Projeto Design-System

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './styles/global.css';
+export function App() {
+  return (
+  <>
+   <h1 className=" bg-cyan-500 font-bold text-2xl text-red-700">hello world</h1>
+   <button className="bg-cyan-300 hover:bg-gray-400">
+    Olá
+   </button>
+   </>
+  )
+}
+
+
