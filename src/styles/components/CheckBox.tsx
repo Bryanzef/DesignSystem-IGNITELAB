56b2ac591f146221ee9eb746421cd6ac2189c15e
Bryanzef <98066200+Bryanzef@users.noreply.github.com>
2022-10-13 17:32:46 -0300

    atualizado

--- src/styles/components/CheckBox.tsx
+++ src/styles/components/CheckBox.tsx
@@ -1,4 +1,19 @@
-import * as CheckboxPrimitive from "@radix-ui/react-checkbox";
+import * as CheckboxPrimitive from '@radix-ui/react-checkbox';
+import { Check } from 'phosphor-react'
+
+export interface CheckboxProps extends CheckboxPrimitive.CheckboxProps {}
+
+export function Checkbox(props: CheckboxProps) {
+  return (
+    <CheckboxPrimitive.Root className="w-6 h-6 p-[2px] bg-gray-800 rounded" {...props}>
+      <CheckboxPrimitive.Indicator asChild>
+        <Check weight="bold" className="h-5 w-5 text-cyan-500" />
+      </CheckboxPrimitive.Indicator>
+    </CheckboxPrimitive.Root>
+  )
+}
+
+/*import * as CheckboxPrimitive from "@radix-ui/react-checkbox";
 import { Check } from "phosphor-react";
 
 export interface CheckboxProps {}
@@ -11,4 +26,4 @@ export function Checkbox({}: CheckboxProps) {
       </CheckboxPrimitive.Indicator>
     </CheckboxPrimitive.Root>
   );
-}
+} */
